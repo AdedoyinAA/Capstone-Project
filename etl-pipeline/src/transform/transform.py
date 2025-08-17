2efc29ec9c9f5b8205684f545d6e959f6e8227f6
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-08-17 22:53:54 +0100

    update: setup logger for transformation process, debugging

--- etl-pipeline/src/transform/transform.py
+++ etl-pipeline/src/transform/transform.py
@@ -3,10 +3,24 @@ from typing import Tuple
 from src.transform.clean_boxscores import clean_boxscores
 from src.transform.clean_games import clean_games
 from src.transform.clean_salaries import clean_salaries
+from src.utils.logging_utils import setup_logger
+
+
+# Configure the logger
+logger = setup_logger("transform_data", "transform_data.log")
 
 
 def transform_data(data) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    cleaned_boxscores = clean_boxscores(data[0])
-    cleaned_games = clean_games(data[1])
-    cleaned_salaries = clean_salaries(data[3])
-    return (cleaned_boxscores, cleaned_games, cleaned_salaries)
+    try:
+        logger.info("Starting data transformation process")
+        cleaned_boxscores = clean_boxscores(data[0])
+        cleaned_games = clean_games(data[1])
+        cleaned_salaries = clean_salaries(data[3])
+        logger.info(
+            "Data transformation completed successfully"
+        )
+        return (cleaned_boxscores, cleaned_games, cleaned_salaries)
+
+    except Exception as e:
+        logger.error(f"Data transformation failed: {e}")
+        raise

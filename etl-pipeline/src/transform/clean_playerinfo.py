b6bac445ca9b2aaaa4415b938afef49ba9d6aaec
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-08-18 10:23:19 +0100

    update: transformed playerinfo with python script

---
+++ etl-pipeline/src/transform/clean_playerinfo.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from src.utils.trimming_whitespace_utils import trim_whitespaces
+from src.utils.remove_special_characters_utils import remove_special_characters
+
+FILE_PATH = "data/processed/cleaned_playerinfo.csv"
+
+
+def clean_playerinfo(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    # Remove unnecessary columns
+    playerinfo = remove_unnecessary_columns(playerinfo)
+    # Rename columns
+    playerinfo = rename_columns(playerinfo)
+    # Remove missing values
+    playerinfo = remove_missing_values(playerinfo)
+    # Change position values
+    playerinfo = change_position_values(playerinfo)
+    # Calculate weight in kg
+    playerinfo = calculate_height_m(playerinfo)
+    # Calculate height in metres
+    playerinfo = calculate_height_m(playerinfo)
+    # Trim whitespaces
+    playerinfo = trim_whitespaces(playerinfo)
+    # Remove special characters from player names
+    playerinfo = remove_special_characters(playerinfo)
+    # Save the cleaned dataframe as a CSV
+    playerinfo.to_csv(FILE_PATH, index=False)
+    return playerinfo
+
+
+def remove_unnecessary_columns(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    columns_to_drop = ["Colleges", "From", "To"]
+    playerinfo = playerinfo.drop(columns=columns_to_drop, axis="columns")
+
+    return playerinfo
+
+
+def rename_columns(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    dict_for_renaming_columns = {
+        "playerName": "player_name",
+        "From": "from ",
+        "To": "to ",
+        "Pos": "position",
+        "Ht": "height",
+        "Wt": "weight",
+        "birthDate": "birth_date",
+        "Colleges": "colleges"
+    }
+    playerinfo = playerinfo.rename(columns=dict_for_renaming_columns)
+
+    return playerinfo
+
+
+def remove_missing_values(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    # Remove rows with null values from the weight column
+    playerinfo = playerinfo.dropna(subset=["weight"])
+
+    # remove rows with null values from the height column
+    playerinfo = playerinfo.dropna(subset=["birth_date"])
+
+    return playerinfo
+
+
+def change_position_values(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    # Create a map to change the values in the position column
+    mapping_dict = {
+        "F-C": "Forward-Center",
+        "C-F": "Center-Forward",
+        "C": "Center",
+        "G": "Guard",
+        "F": "Forward",
+        "G-F": "Guard-Forward",
+        "F-G": "Forward-Guard"
+    }
+    playerinfo["position"] = playerinfo["position"].replace(mapping_dict)
+
+    return playerinfo
+
+
+def calculate_weight_kg(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    # Add a new column which calculates the player's weight in kilograms(kg)
+    playerinfo["weight_kg"] = (
+        playerinfo["weight"] * 0.45359237
+    ).round(1)
+
+    return playerinfo
+
+
+# Function to convert "feet-inches" to metres in height column
+def height_to_metres(height_string):
+    try:
+        # Save the feet and inches values in a list
+        feet, inches = map(int, height_string.split("-"))
+        # Find the total value of inches
+        total_inches = feet * 12 + inches
+        # Convert to metres rounded to 2 decimal places
+        return round(total_inches * 0.0254, 2)
+    except Exception:
+        return None
+
+
+def calculate_height_m(playerinfo: pd.DataFrame) -> pd.DataFrame:
+    # Create the new column
+    playerinfo["height_m"] = playerinfo["height"].apply(height_to_metres)
+
+    return playerinfo
--- etl-pipeline/src/transform/transform.py
+++ etl-pipeline/src/transform/transform.py
@@ -2,6 +2,7 @@ import pandas as pd
 from typing import Tuple
 from src.transform.clean_boxscores import clean_boxscores
 from src.transform.clean_games import clean_games
+from src.transform.clean_playerinfo import clean_playerinfo
 from src.transform.clean_salaries import clean_salaries
 from src.utils.logging_utils import setup_logger
 
@@ -15,11 +16,17 @@ def transform_data(data) -> Tuple[pd.DataFrame, pd.DataFrame]:
         logger.info("Starting data transformation process")
         cleaned_boxscores = clean_boxscores(data[0])
         cleaned_games = clean_games(data[1])
+        cleaned_playerinfo = clean_playerinfo(data[2])
         cleaned_salaries = clean_salaries(data[3])
         logger.info(
             "Data transformation completed successfully"
         )
-        return (cleaned_boxscores, cleaned_games, cleaned_salaries)
+        return (
+            cleaned_boxscores,
+            cleaned_games,
+            cleaned_playerinfo,
+            cleaned_salaries
+        )
 
     except Exception as e:
         logger.error(f"Data transformation failed: {e}")

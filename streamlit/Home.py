c5e9674b989c8e7ea801e74f30e4fc7f2521fda3
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-08-19 14:23:06 +0100

    new: setup streamlit application and pages

---
+++ streamlit/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="HoopMetrics",
+    layout="wide"
+)
+
+st.title("HoopMetrics! 🏀")
---
+++ streamlit/pages/1_Player_Statistics.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Player Stats",
+    layout="wide"
+)
+
+st.title("Player Statistics ⛹️")
